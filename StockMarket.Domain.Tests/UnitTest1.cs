b561cc37c4442a0368907fffb01cc422b18bba5c
Parisa <mousaviparisa5@yahoo.com>
2023-04-20 15:39:32 +0330

    Add project files.

---
+++ StockMarket.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StockMarket.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ StockMarket.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StockMarket.Domain
+{
+    public class Class1
+    {
+
+    }
+}
